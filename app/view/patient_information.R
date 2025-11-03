6cb9c361a897f28b930d999dc8f021d83c50e4aa
Romain Garreau <romain.garreau@univ-lyon1.fr>
2025-11-03 17:41:41 +0100

    update UI

--- app/view/patient_information.R
+++ app/view/patient_information.R
@@ -1,18 +1,19 @@
 box::use(
-  shiny[column, dateInput, fluidPage, fluidRow, selectInput, selectizeInput, 
-        tagList, textInput, tags, icon],
-  bs4Dash[tabItem, box],
+  shiny[column, dateInput, fluidRow, selectInput, selectizeInput, tabPanel, tagList, textInput, tags, icon, NS, moduleServer, reactive],
+  bs4Dash[box],
 )
 
 #' Patient Information Tab UI
 #'
+#' @param id Module ID
 #' @param translator shiny.i18n Translator object
 #' @export
-patient_information_ui <- function(translator) {
-  tabItem(
-    tabName = "information",
-    fluidPage(
-      fluidRow(
+ui <- function(id, translator) {
+  ns <- NS(id)
+  
+  tabPanel(
+    translator$t("Informations du patient"),
+    fluidRow(
         column(
           width = 3,
           box(
@@ -22,29 +23,46 @@ patient_information_ui <- function(translator) {
             solidHeader = TRUE,
             collapsible = FALSE,
             fluidRow(
-              column(width = 6, textInput(inputId = "first_name", label = translator$t("Prénom"), width = "100%")),
-              column(width = 6, textInput(inputId = "last_name", label = translator$t("Nom"), width = "100%"))
+              column(width = 6, textInput(inputId = ns("first_name"), label = translator$t("Prénom"), width = "100%")),
+              column(width = 6, textInput(inputId = ns("last_name"), label = translator$t("Nom"), width = "100%"))
             ),
             fluidRow(
               tags$style(type = "text/css", ".datepicker { z-index: 99999 !important; }"),
-              column(width = 6, dateInput("birthdate", label = translator$t("Date de naissance"), 
-                                         format = "yyyy-mm-dd", value = Sys.Date(), language = "fr")),
-              column(width = 6, selectInput(inputId = "sex", label = translator$t("Sexe"), 
-                                           choices = c(translator$t("Masculin"), translator$t("Féminin"))))
+              column(width = 6, dateInput(ns("birthdate"), label = translator$t("Date de naissance"), format = "yyyy-mm-dd", value = Sys.Date(), language = "fr")),
+              column(width = 6, selectInput(inputId = ns("sex"), label = translator$t("Sexe"), choices = c(translator$t("Masculin"), translator$t("Féminin"))))
             ),
             fluidRow(
-              column(width = 6, selectizeInput(inputId = "hospital", label = translator$t("Hôpital"), 
-                                              choices = c("HCL", "CHU"), options = list(create = TRUE))),
-              column(width = 6, textInput(inputId = "ward", label = translator$t("Service")))
+              column(width = 6, selectizeInput(inputId = ns("hospital"), label = translator$t("Hôpital"), choices = c("HCL", "CHU"), options = list(create = TRUE))),
+              column(width = 6, textInput(inputId = ns("ward"), label = translator$t("Service")))
             ),
             fluidRow(
-              column(width = 6, selectInput(inputId = "drug", label = translator$t("Médicament"), 
-                                           choices = c("Amikacin", "Vancomycin", "Gentamicin"), width = "100%")),
-              column(width = 6, textInput(inputId = "phone_number", label = translator$t("Numéro de téléphone")))
+              column(width = 6, selectInput(inputId = ns("drug"), label = translator$t("Médicament"), choices = c("Amikacin", "Vancomycin", "Gentamicin"), width = "100%")),
+              column(width = 6, textInput(inputId = ns("phone_number"), label = translator$t("Numéro de téléphone")))
             )
           )
         )
       )
     )
-  )
+}
+
+#' Patient Information Tab Server
+#'
+#' @param id Module ID
+#' @export
+server <- function(id) {
+  moduleServer(id, function(input, output, session) {
+    # Return reactive values for use by other modules
+    return(reactive({
+      list(
+        first_name = input$first_name,
+        last_name = input$last_name,
+        birthdate = input$birthdate,
+        sex = input$sex,
+        hospital = input$hospital,
+        ward = input$ward,
+        drug = input$drug,
+        phone_number = input$phone_number
+      )
+    }))
+  })
 }
