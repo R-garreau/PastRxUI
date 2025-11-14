20bacabd68a19211b5d26f280002f09be3b73f47
Romain Garreau <romain.garreau@chu-lyon.fr>
2025-11-14 15:07:36 +0100

    add Weight works
    - user can add weight (tbw or other)
    - calculate alternative metrics, tbw and bsa
    - validation in case multiple instance of weight are recorded at the same time

--- app/logic/validators.R
+++ app/logic/validators.R
@@ -1,91 +1,102 @@
-#' Initialize all input validators
-#'
-#' @param input Shiny input object
-#' @return List of enabled validators
-#' @noRd
-initialize_validators <- function(input) {
-  validators <- list()
+box::use(
+  shiny[showNotification],
+)
 
-  # Birthdate validation
-  validators$iv <- create_birthdate_validator()
+box::use(
+  app/logic/utils[calc_age, is_unique],
+)
 
-  # Renal function validation
-  validators$renal_iv <- create_renal_validator(input)
 
-  # BestDose route validation
-  validators$bd_iv <- create_bestdose_route_validator()
 
-  # Drug validation
-  validators$drug_validator <- create_drug_validator()
 
-  # Report validation
-  validators$report_validator <- create_report_validator()
+# #' Initialize all input validators
+# #'
+# #' @param input Shiny input object
+# #' @return List of enabled validators
+# #' @export
+# initialize_validators <- function(input) {
+#   validators <- list()
 
-  return(validators)
-}
+#   # Birthdate validation
+#   validators$iv <- create_birthdate_validator()
 
-#' Create birthdate validator
-#' @return InputValidator object
-#' @noRd
-create_birthdate_validator <- function() {
-  iv <- InputValidator$new()
-  iv$add_rule("birthdate", ~ if (. == Sys.Date()) "Enter the real birthdate")
-  iv$enable()
-  return(iv)
-}
+#   # Renal function validation
+#   validators$renal_iv <- create_renal_validator(input)
 
-#' Create renal function validator
-#' @param input Shiny input object
-#' @return InputValidator object
-#' @noRd
-create_renal_validator <- function(input) {
-  renal_iv <- InputValidator$new()
-  renal_iv$condition(~ input$eGFR == "schwartz")
-  renal_iv$add_rule("eGFR", ~ if (calc_age(input$birthdate) < 18) "Warning. Schwartz is only valid for patient < 18 years")
-  renal_iv$enable()
-  return(renal_iv)
-}
+#   # BestDose route validation
+#   validators$bd_iv <- create_bestdose_route_validator()
 
-#' Create BestDose route validator
-#' @return InputValidator object
-#' @noRd
-create_bestdose_route_validator <- function() {
-  bd_iv <- InputValidator$new()
-  bd_iv$add_rule("administration_route", ~ if (. == "SC") "Subcutaneous Route isn't supported by BestDose, Use IM instead")
-  bd_iv$enable()
-  return(bd_iv)
-}
+#   # Drug validation
+#   validators$drug_validator <- create_drug_validator()
 
-#' Create drug validator
-#' @return InputValidator object
-#' @noRd
-create_drug_validator <- function() {
-  drug_validator <- InputValidator$new()
-  drug_validator$add_rule("drug", ~ if (. == "") "Please select a drug before saving")
-  drug_validator$add_rule("first_name", sv_required())
-  drug_validator$add_rule("last_name", sv_required())
-  drug_validator$add_rule("ward", sv_required())
-  drug_validator$enable()
-  return(drug_validator)
-}
+#   # Report validation
+#   validators$report_validator <- create_report_validator()
 
-#' Create report validator
-#' @return InputValidator object
-#' @noRd
-create_report_validator <- function() {
-  report_validator <- InputValidator$new()
-  report_validator$add_rule("pharmacologist", sv_required(message = "Value Required to create report"))
-  report_validator$add_rule("medical_indication", sv_required(message = "Value Required to create report"))
-  report_validator$enable()
-  return(report_validator)
-}
+#   return(validators)
+# }
+
+# #' Create birthdate validator
+# #' @return InputValidator object
+# #' @export
+# create_birthdate_validator <- function() {
+#   iv <- InputValidator$new()
+#   iv$add_rule("birthdate", ~ if (. == Sys.Date()) "Enter the real birthdate")
+#   iv$enable()
+#   return(iv)
+# }
+
+# #' Create renal function validator
+# #' @param input Shiny input object
+# #' @return InputValidator object
+# #' @export
+# create_renal_validator <- function(input) {
+#   renal_iv <- InputValidator$new()
+#   renal_iv$condition(~ input$eGFR == "schwartz")
+#   renal_iv$add_rule("eGFR", ~ if (calc_age(input$birthdate) < 18) "Warning. Schwartz is only valid for patient < 18 years")
+#   renal_iv$enable()
+#   return(renal_iv)
+# }
+
+# #' Create BestDose route validator
+# #' @return InputValidator object
+# #' @export
+# create_bestdose_route_validator <- function() {
+#   bd_iv <- InputValidator$new()
+#   bd_iv$add_rule("administration_route", ~ if (. == "SC") "Subcutaneous Route isn't supported by BestDose, Use IM instead")
+#   bd_iv$enable()
+#   return(bd_iv)
+# }
+
+# #' Create drug validator
+# #' @return InputValidator object
+# #' @export
+# create_drug_validator <- function() {
+#   drug_validator <- InputValidator$new()
+#   drug_validator$add_rule("drug", ~ if (. == "") "Please select a drug before saving")
+#   drug_validator$add_rule("first_name", sv_required())
+#   drug_validator$add_rule("last_name", sv_required())
+#   drug_validator$add_rule("ward", sv_required())
+#   drug_validator$enable()
+#   return(drug_validator)
+# }
+
+# #' Create report validator
+# #' @return InputValidator object
+# #' @export
+# create_report_validator <- function() {
+#   report_validator <- InputValidator$new()
+#   report_validator$add_rule("pharmacologist", sv_required(message = "Value Required to create report"))
+#   report_validator$add_rule("medical_indication", sv_required(message = "Value Required to create report"))
+#   report_validator$enable()
+#   return(report_validator)
+# }
 
 
 #' Validate unique times and show warning if duplicates found
 #'
 #' @param time_vector Vector of time values to check
 #' @param time_type String describing the type of time (e.g., "administration", "weight", "concentration")
-#' @noRd
+#' @export
 validate_unique_times <- function(time_vector, time_type) {
   if (!is_unique(time_vector)) {
     message <- paste0(
@@ -98,6 +109,6 @@ validate_unique_times <- function(time_vector, time_type) {
     } else if (time_type == "concentration") {
       message <- "Some concentrations have identical sampling time. This may cause a crash when using BestDose. Please fix it"
     }
-     # replace with a shiny modal notify(content = message, type = "warning")
+    showNotification(message, type = "warning")
   }
 }
---
+++ dev/todo.md
@@ -0,0 +1,6 @@
+## toolbox
+- [ ] split toolbox 
+  - [ ] "Use mod weight"/ Use BSA are only for the file to be computed" => create report option in header or open a modal to chose option when creating the mb2 file
+  - [ ] other option should be in the administration tab
+  - [ ] renal calculator shoulb be an option in the lab values
+- [ ] Remove Phone Number in patient information
