7349e9dd322c6c3e1f25c7bdb5f763d9c5400a3a
Romain Garreau <romain.garreau@chu-lyon.fr>
2025-11-16 20:32:22 +0100

    Update documentation.R
    fix display issue

--- app/view/documentation.R
+++ app/view/documentation.R
@@ -1,5 +1,5 @@
 box::use(
-  shiny[includeMarkdown, NS, tags, tabPanel],
+  shiny[includeMarkdown, NS, tags, tabPanel, moduleServer],
   bs4Dash[tabsetPanel],
 )
 
@@ -10,19 +10,22 @@ box::use(
 ui <- function(id, i18n) {
   ns <- NS(id)
 
-  tabsetPanel(
-    id = ns("documentation_tabs"),
-    vertical = TRUE,
-    tabPanel(
-      title = "Functionality",
-      tags$div(
-        includeMarkdown("app/documentation/added_functionality.md")
-      )
-    ),
-    tabPanel(
-      title = "Options Explanation",
-      tags$div(
-        includeMarkdown("app/documentation/options_explanation.md")
+  tabPanel(
+    i18n$translate("Documentation"),
+    tabsetPanel(
+      id = ns("documentation_tabs"),
+      vertical = TRUE,
+      tabPanel(
+        title = "Functionality",
+        tags$div(
+          includeMarkdown("app/documentation/added_functionality.md")
+        )
+      ),
+      tabPanel(
+        title = "Options Explanation",
+        tags$div(
+          includeMarkdown("app/documentation/options_explanation.md")
+        )
       )
     )
   )
